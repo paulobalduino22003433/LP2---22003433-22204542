15f6fae73cc2ba3c8df3f8b5ba4cf2fada36594c
paulo-balduino <101064225+paulobalduino22003433@users.noreply.github.com>
2023-11-04 19:41:02 +0000

    Delete src/pt/ulusofona/lp2/deisichess/JogadasWhiteTeam.java
    Nao é permitido o uso de extend

--- src/pt/ulusofona/lp2/deisichess/JogadasWhiteTeam.java
+++
@@ -1,9 +0,0 @@
-package pt.ulusofona.lp2.deisichess;
-
-public class JogadasWhiteTeam extends JogadasBlackTeam {
-
-    @Override
-    public String calculaCaptura(Tabuleiro tabuleiro, int nrPeca) {
-        return ((nrPeca/2) - tabuleiro.whiteTeam.size()) + ""; //Ou seja, o nr total de pecas brancas - o nr de peca no tabuleiro. O resultado é q quantidade de pecas capturadas.
-    }
-}
