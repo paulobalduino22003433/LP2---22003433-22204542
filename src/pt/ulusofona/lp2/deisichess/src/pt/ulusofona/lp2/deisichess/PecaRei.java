589aac55269b3e2f370b99e405247e71d32c7856
paulo-balduino-a22003433 <101064225+paulobalduino22003433@users.noreply.github.com>
2023-11-29 19:48:33 +0000

    Delete src directory

--- src/pt/ulusofona/lp2/deisichess/src/pt/ulusofona/lp2/deisichess/PecaRei.java
+++
@@ -1,9 +0,0 @@
-package pt.ulusofona.lp2.deisichess;
-
-public class PecaRei extends Peca {
-
-    public PecaRei(String identificador, String tipoDePeca, String equipa, String alcunha) {
-        super(identificador, tipoDePeca, equipa, alcunha);
-    }
-
-}
