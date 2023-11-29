8d046e78b0f4cc627e180bc9e0af3803a6758123
paulo-balduino-a22003433 <101064225+paulobalduino22003433@users.noreply.github.com>
2023-11-29 21:15:51 +0000

    Peca rei

--- src/pt/ulusofona/lp2/deisichess/PecaRei.java
+++ src/pt/ulusofona/lp2/deisichess/PecaRei.java
@@ -3,19 +3,19 @@ package pt.ulusofona.lp2.deisichess;
 public class PecaRei extends Peca {
     public int pontos = 1000 ;
     public PecaRei(String identificador, String tipoDePeca, String equipa, String alcunha) {
-        super(identificador, tipoDePeca.equals("0") ? "Rei" : tipoDePeca, equipa, alcunha);
+        super(identificador, tipoDePeca, equipa, alcunha);
     }
     @Override
     public String toString() {
         if(estado.equals("capturado")){
             if (tipoDePeca.equals("0")){
-                return identificador + " | " + tipoDePeca + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + "n" + "/" + "a" + ")";
+                return identificador + " | " + "Rei" + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + "n" + "/" + "a" + ")";
             }else{
                 return super.toString();
             }
         }
         if (tipoDePeca.equals("0")){
-            return identificador + " | " + tipoDePeca + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + x + ", " + y + ")";
+            return identificador + " | " + "Rei" + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + x + ", " + y + ")";
         }else {
             return super.toString();
         }
