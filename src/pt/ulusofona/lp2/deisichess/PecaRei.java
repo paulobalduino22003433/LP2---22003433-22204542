541ff36cc64121619f6578bb8adf0517e57d46b5
paulo-balduino-a22003433 <101064225+paulobalduino22003433@users.noreply.github.com>
2023-11-29 21:06:21 +0000

    Mudanças Rei

--- src/pt/ulusofona/lp2/deisichess/PecaRei.java
+++ src/pt/ulusofona/lp2/deisichess/PecaRei.java
@@ -1,9 +1,23 @@
 package pt.ulusofona.lp2.deisichess;
 
 public class PecaRei extends Peca {
-
+    public int pontos = 1000 ;
     public PecaRei(String identificador, String tipoDePeca, String equipa, String alcunha) {
-        super(identificador, tipoDePeca, equipa, alcunha);
+        super(identificador, tipoDePeca.equals("0") ? "Rei" : tipoDePeca, equipa, alcunha);
     }
-
+    @Override
+    public String toString() {
+        if(estado.equals("capturado")){
+            if (tipoDePeca.equals("0")){
+                return identificador + " | " + tipoDePeca + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + "n" + "/" + "a" + ")";
+            }else{
+                return super.toString();
+            }
+        }
+        if (tipoDePeca.equals("0")){
+            return identificador + " | " + tipoDePeca + " | " + "(infinito) | " + equipa + " | " + alcunha + " @ (" + x + ", " + y + ")";
+        }else {
+            return super.toString();
+        }
+    }
 }
