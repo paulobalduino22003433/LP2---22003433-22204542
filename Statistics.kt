6b8ccf7ce19d404d28891eba63e9f42125917e49
paulo-balduino-a22003433 <101064225+paulobalduino22003433@users.noreply.github.com>
2023-12-26 19:03:24 +0000

    Rei/Rainha changes

---
+++ Statistics.kt
@@ -0,0 +1,35 @@
+package pt.ulusofona.lp2.deisichess
+
+
+    fun getStatsCalculator(statType: StatType): (GameManager) -> List<String> {
+        return when (statType) {
+            StatType.TOP_5_CAPTURAS -> { gameManager -> calculateTop5Capturas(gameManager) }
+            StatType.TOP_5_PONTOS -> { gameManager -> calculateTop5Pontos(gameManager) }
+            StatType.PECAS_MAIS_5_CAPTURAS -> { gameManager -> calculatePecasMais5Capturas(gameManager) }
+            StatType.PECAS_MAIS_BARALHADAS -> { gameManager -> calculatePecasMaisBaralhadas(gameManager) }
+            StatType.TIPOS_CAPTURADOS -> { gameManager -> calculateTiposCapturados(gameManager) }
+        }
+    }
+
+    private fun calculateTop5Capturas(gameManager: GameManager): List<String> {
+        return listOf("Result for TOP_5_CAPTURAS")
+    }
+
+    private fun calculateTop5Pontos(gameManager: GameManager): List<String> {
+        return listOf("Result for TOP_5_PONTOS")
+    }
+
+    private fun calculatePecasMais5Capturas(gameManager: GameManager): List<String> {
+        return listOf("Result for PECAS_MAIS_5_CAPTURAS")
+    }
+
+    private fun calculatePecasMaisBaralhadas(gameManager: GameManager): List<String> {
+        return listOf("Result for PECAS_MAIS_BARALHADAS")
+    }
+
+    private fun calculateTiposCapturados(gameManager: GameManager): List<String> {
+        return listOf("Result for TIPOS_CAPTURADOS")
+    }
+
+
+
