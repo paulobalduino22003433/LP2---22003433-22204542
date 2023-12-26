24042add7bdf46abfb6a5d9b1240d8b4ae34b84d
paulo-balduino-a22003433 <101064225+paulobalduino22003433@users.noreply.github.com>
2023-12-26 03:48:46 +0000

    getHints

--- src/pt/ulusofona/lp2/deisichess/Hint.java
+++ src/pt/ulusofona/lp2/deisichess/Hint.java
@@ -20,6 +20,6 @@ public class Hint implements Comparable<Hint> {
 
     @Override
     public String toString() {
-        return "(" + x + "," + y + ")->" + points;
+        return "(" + x + "," + y + ") -> " + points;
     }
 }
